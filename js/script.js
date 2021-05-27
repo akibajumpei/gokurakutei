e079e6e7f816197c731de1fd15058ee0023dac34
akibajumpei <84856358+akibajumpei@users.noreply.github.com>
2021-05-27 09:20:11 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -47,5 +47,14 @@ $(function(){
             $(".hide-menu").fadeOut();
         }
     });
+    
+    $('a[href^="#end"]').click(function(){
+        var speed = 500;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+    });
 
 });
